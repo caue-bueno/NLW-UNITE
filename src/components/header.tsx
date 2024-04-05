767e4d7b2b45058d339c5959a8f9a4216898fde0
caue-bueno <caue-bueno@live.com>
2024-04-05 17:00:06 -0300

    Finish layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,14 @@
 import nlwicon from '../assets/nlw-icon.svg'
+import { NavLink } from './nav-link'
 
 export function Header() {
   return (
     <div className='flex items-center gap-5 p-2'>
-      <img src={nlwicon} className='h-10 w-10'/>
+      <img src={nlwicon} className='h-10 w-10' />
 
       <nav className='flex items-center gap-5'>
-        <a href="" className='font-medium text-sm text-zinc-300'>Eventos</a>
-        <a href="" className='font-medium text-sm'>Participantes</a>
+        <NavLink href="/eventos" >Eventos</NavLink>
+        <NavLink href="/participantes">Participantes</NavLink>
       </nav>
     </div>
   )
